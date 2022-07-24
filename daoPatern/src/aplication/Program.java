ccd3a5f68d7a72a5a5d45c34c2fb599413f4bd74
Pablo Torres <pablotorreeifel@gmail.com>
2022-07-24 16:27:12 -0300

    SellerDaoJDBC and DaoFactory

--- daoPatern/src/aplication/Program.java
+++ daoPatern/src/aplication/Program.java
@@ -4,6 +4,8 @@ import java.sql.Connection;
 import java.util.Date;
 
 import db.DB;
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -13,7 +15,10 @@ public class Program {
 		Connection conn = DB.getConnection();
 		
 		Department obj = new Department(1, "Books");
+		
 		Seller seller = new Seller(1, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
 		System.out.println(seller);
 
